5ef9f9b06837d1666c33d3626241194f088e5edd
Mahmoud Elrmly <mahmoud.el912@gmail.com>
2023-10-10 23:13:16 -0600

    Embedded JS in ISX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import * as React from 'react';
 
+const title = 'React';
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {title}</h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
     </div>
   );
 }
